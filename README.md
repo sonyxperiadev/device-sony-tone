7a2d59ca226c25baf637abc618f32272c7b995d5
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-09-30 10:08:57 +0200

    inital configuration
    initial configuration for Sony tone platform (msm8996)
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# device-sony-tone
+Copyright (C) Sony Mobile Communications 2016
+=============================================
+
+This is the Android device configuration for tone platform.
+
+tone devices:
+
+    Xperia X Performance.................Dora
+
+Build instructions
+http://developer.sonymobile.com/knowledge-base/open-source/open-devices/aosp-build-instructions/
---
+++ rootdir/Android.mk
@@ -0,0 +1,53 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.tone
+LOCAL_SRC_FILES := fstab.tone
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := fstab.tone
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := init.tone.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.$(TARGET_DEVICE)
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.tone.pwr
+LOCAL_SRC_FILES := init.tone.pwr.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.tone.pwr
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.recovery.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := init.recovery.tone.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.recovery.$(TARGET_DEVICE)
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ueventd.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := ueventd.tone.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := ueventd.$(TARGET_DEVICE)
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+$(shell mkdir -p $(PRODUCT_OUT)/root && pushd $(PRODUCT_OUT)/root > /dev/null && ln -s fstab.tone fstab.$(TARGET_DEVICE) && popd > /dev/null)
+
