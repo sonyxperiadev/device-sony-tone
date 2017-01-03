sonyxperiadev/device-sony-loire

loire: Update kernel address
Matching new bootloaders

Signed-off-by: David Viteri <davidteri91@gmail.com>

PlatformConfig.mk
@@ -32,7 +32,7 @@ TARGET_USES_64_BIT_BCMDHD := true
 
 ENABLE_CPUSETS := true
 
-BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_BASE        := 0x20000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000

sonyxperiadev/device-sony-tone

PlatformConfig.mk
@@ -34,7 +34,7 @@ TARGET_USES_64_BIT_BCMDHD := true
 
 ENABLE_CPUSETS := true
 
-BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000