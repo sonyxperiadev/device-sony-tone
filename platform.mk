sonyxperiadev/device-sony-loire

Add setting overlay for fingerprint location

platform.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/loire/overlay
+
 # Platform Init
 PRODUCT_PACKAGES += \
     fstab.loire \

sonyxperiadev/device-sony-tone

platform.mk
@@ -56,6 +56,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/tone/overlay
+
 # Platform Init
 PRODUCT_PACKAGES += \
     fstab.tone \