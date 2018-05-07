09dcc829f82fd8a62231447666656c82314d9c5b
Alin Jerpelea <alin.jerpelea@sony.com>
2018-05-07 17:27:14 +0200

    remove unused keystore package
    The keystore package is deprecated and replaced by HILD
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -91,10 +91,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.msm8996
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    keystore.msm8996
-
 # Fluence
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence
