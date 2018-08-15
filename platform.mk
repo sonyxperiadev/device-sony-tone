1abc1d3d88d9c09ebb1ec8982f5a4cc9db4aba02
Alin Jerpelea <alin.jerpelea@sony.com>
2018-08-15 12:00:14 +0200

    switch the platform to 4.9 kernel
    For Android 9 we will use the kernel 4.9 as default
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
 SOMC_PLATFORM := tone
-SOMC_KERNEL_VERSION := 4.4
+SOMC_KERNEL_VERSION := 4.9
 
 SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
 
