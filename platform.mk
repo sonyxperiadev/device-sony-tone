d7778db98b8e4bef80866734a92d98f625bc8fb1
ix5 <ix5@users.noreply.github.com>
2020-04-22 20:03:19 +0200

    platform: Sync video props
    The ConfigStore HAL is being deprecated and will apparently
    be removed in R.
    
    Translate the uppercase makevars that surfaceflinger.mk used
    to supply as cflags to configstore internals to sysprops.
    
    max_frame_buffer_acquired_buffers = NUM_FRAMEBUFFER_SURFACE_BUFFERS
    
    Note: The uppercase vars must be kept because
    hardware/qcom/display still relies on them at build time.

--- platform.mk
+++ platform.mk
@@ -154,6 +154,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.als_scale=1 \
     ro.qfusion_use_report_period=false
 
+# SurfaceFlinger
+# Keep in sync with NUM_FRAMEBUFFER_SURFACE_BUFFERS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
 # Display HACK: Use GPU composition only
 ## TEMP: Skip loading libsdmextension.so in display hal
 PRODUCT_PROPERTY_OVERRIDES += \
